046b24faafc6d5e4cb27b2fc02ee53fda93ddf02
nhamilton1 <nickh3592@gmail.com>
2022-01-06 21:07:13 -0500

    cagr chart made and formula fixed

--- src/components/CagrChart.js
+++ src/components/CagrChart.js
@@ -1,14 +1,77 @@
 import React from 'react'
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Line }            from 'react-chartjs-2'
+
 
 const CagrChart = props => {
     const { historicPriceRange } = props
 
     console.log(historicPriceRange)
 
+    const labels = historicPriceRange.map(x => x.btc_date)
+    const btc_price_data = historicPriceRange.map(x => x.btc_price)
+    const gld_price_data = historicPriceRange.map(x => x.gld_price)
+    const spy_price_data = historicPriceRange.map(x => x.spy_price)
+
+    const options = {
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: 'Title goes here',
+        },
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+          grid: {
+            drawOnChartArea: false,
+          },
+        },
+      },
+    };
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'BTC',
+          data: btc_price_data,
+          borderColor: 'rgb(255,153,0)',
+          backgroundColor: 'rgba(255,153,0)',
+          yAxisID: 'y',
+        },
+        {
+          label: 'GLD',
+          data: gld_price_data,
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235)',
+          yAxisID: 'y1',
+        },
+        {
+          label: 'SPY',
+          data: spy_price_data,
+          borderColor: 'rgb(255,160,122)',
+          backgroundColor: 'rgba(255,160,122)',
+          yAxisID: 'y1',
+        }
+      ]
+    }
+
     return (
-      <div>
-        
-      </div>
+      <Line data={data} options={options}/>
     )
 }
 
--- src/components/CagrValue.js
+++ src/components/CagrValue.js
@@ -1,61 +1,65 @@
-import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
-import { Card, Statistic } from 'antd'
-import React from 'react'
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Statistic } from 'antd';
+import React from 'react';
 
 const CagrValue = props => {
-    const { historicPriceRange } = props
-    
-    const cagrFormula = (endVal, beginningVal, numOfYears) => 
-    `${(((Math.pow((endVal / beginningVal), 1)) - numOfYears)*100).toFixed(2)}%`
+  const { historicPriceRange } = props;
 
-    let numOfYears = historicPriceRange.length/365
-    //bitcoin cagr values
-    let btcEndVal = historicPriceRange[historicPriceRange.length - 1].btc_price
-    let btcBeginningVal = historicPriceRange[0].btc_price
-    const btcCagrVal = cagrFormula(btcEndVal, btcBeginningVal, numOfYears)
+  const cagrFormula = (endVal, beginningVal, numOfYears) =>
+    `${((Math.pow(endVal / beginningVal, 1/numOfYears) - 1) * 100).toFixed(2)}%`;
 
-    //gld cagr values
-    let gldEndVal = historicPriceRange[historicPriceRange.length - 1].gld_price
-    let gldBeginningVal = historicPriceRange[0].gld_price
-    const gldCagrVal = cagrFormula(gldEndVal, gldBeginningVal, numOfYears)
+  let numOfYears = historicPriceRange.length / 365;
+  //bitcoin cagr values
+  let btcEndVal = historicPriceRange[historicPriceRange.length - 1].btc_price;
+  let btcBeginningVal = historicPriceRange[0].btc_price;
+  const btcCagrVal = cagrFormula(btcEndVal, btcBeginningVal, numOfYears);
 
-    //spy cagr values
-    let spyEndVal = historicPriceRange[historicPriceRange.length - 1].spy_price
-    let spyBeginningVal = historicPriceRange[0].spy_price
-    const spyCagrVal = cagrFormula(spyEndVal, spyBeginningVal, numOfYears)    
-    
-    
-    return (
-      <>
-        <Card  size="small">
-          <Statistic
-            title={`BTC CAGR`}
-            value={btcCagrVal}
-            precision={2}
-            valueStyle={{ color: '#3f8600' }}
-            prefix={<ArrowUpOutlined />}
-          />
-        </Card>
-        <Card  size="small">
-          <Statistic
-            title={`GLD CAGR`}
-            value={gldCagrVal}
-            precision={2}
-            valueStyle={gldCagrVal < 0 ? {color: '#3f8600'} : {color: 'red'}}
-            prefix={gldCagrVal < 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
-          />
-        </Card>
-        <Card  size="small">
-          <Statistic
-            title={`SPY CAGR`}
-            value={spyCagrVal}
-            precision={2}
-            valueStyle={spyCagrVal < 0 ? {color: '#3f8600'} : {color: 'red'}}
-            prefix={spyCagrVal < 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined/>}
-          />
-        </Card>
-      </>
-    )
-}
+  let x = 1
+  //gld cagr values
+  let gldEndVal = historicPriceRange[historicPriceRange.length - x].gld_price;
+  while(historicPriceRange[historicPriceRange.length - x].gld_price === null){
+    gldEndVal = historicPriceRange[historicPriceRange.length - ++x].gld_price
+  }
+  let gldBeginningVal = historicPriceRange[0].gld_price;
+  const gldCagrVal = cagrFormula(gldEndVal, gldBeginningVal, numOfYears);
 
-export default CagrValue
+  //spy cagr values
+  let spyEndVal = historicPriceRange[historicPriceRange.length - x].spy_price;
+  let spyBeginningVal = historicPriceRange[0].spy_price;
+  const spyCagrVal = cagrFormula(spyEndVal, spyBeginningVal, numOfYears);
+
+
+  return (
+    <>
+      <Card size="small">
+        <Statistic
+          title={`BTC CAGR`}
+          value={btcCagrVal}
+          precision={2}
+          valueStyle={{ color: '#3f8600' }}
+          prefix={<ArrowUpOutlined />}
+        />
+      </Card>
+      <Card size="small">
+        <Statistic
+          title={`GLD CAGR`}
+          value={gldCagrVal}
+          precision={2}
+          valueStyle={gldCagrVal < 0 ? { color: '#3f8600' } : { color: 'red' }}
+          prefix={gldCagrVal < 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+        />
+      </Card>
+      <Card size="small">
+        <Statistic
+          title={`SPY CAGR`}
+          value={spyCagrVal}
+          precision={2}
+          valueStyle={spyCagrVal < 0 ? { color: '#3f8600' } : { color: 'red' }}
+          prefix={spyCagrVal < 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+        />
+      </Card>
+    </>
+  );
+};
+
+export default CagrValue;
